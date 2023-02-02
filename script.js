66062f8c43bd20cb99bb365d9874a9c8558d6ec6
abhishek nayak <abhishek5nayak22@gmail.com>
2023-02-02 15:21:07 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 
+
+API Routes
+
+Discover 
+https://api.themoviedb.org/3/discover/movie?sort_by=opularity.desc&api_key=
+
+Search 
+https://api.themoviedb.org/3/search/movie?api_key=""&query=""
+
+*/
+
+
